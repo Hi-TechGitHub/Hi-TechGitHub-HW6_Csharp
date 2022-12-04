38e8cb662f05cef4e6e6a174bcd2943742556633
Hi-TechGitHub <116220279+Hi-TechGitHub@users.noreply.github.com>
2022-12-05 01:32:27 +0300

    all is ready

---
+++ task41/Program.cs
@@ -0,0 +1,47 @@
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+// 0, 7, 8, -2, -2 -> 2
+
+// -1, -7, 567, 89, 223-> 3
+
+int[] FillArray(int size)
+{
+    int[] arr = new int[size];
+    for (int i = 0; i < arr.Length; i++)
+    {
+        Console.Write($"Введите элемент под индексом {i}: ");
+        arr[i] = Convert.ToInt32(Console.ReadLine());
+
+    }
+    return arr;
+}
+void PrintArray(int[] array)
+{
+    Console.Write("[");
+    for (int i = 0; i < array.Length; i += 1)
+    {
+
+        if (i < array.Length - 1) Console.Write($"{array[i]}, ");
+        else Console.Write($"{array[i]}");
+
+    }
+    Console.WriteLine("]");
+
+}
+int MoreZero(int[] array)
+{
+    int counter = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] > 0) counter++;
+
+    }
+    return counter;
+}
+Console.WriteLine("Сколько хотите ввести цифр?");
+int num = Convert.ToInt32(Console.ReadLine());
+int[] array = FillArray(num);
+PrintArray(array);
+
+int res = MoreZero(array);
+Console.Write($" --> {res}");
---
+++ task43/Program.cs
@@ -0,0 +1,17 @@
+﻿void Coordinats()
+{
+    Console.WriteLine("Введите координаты 1 точки, b1, k1");
+    Console.Write("b1 = ");
+    double c = Convert.ToDouble(Console.ReadLine()); 
+    Console.Write("k1 = ");
+    double a = Convert.ToDouble(Console.ReadLine());
+    Console.WriteLine("Введите координаты 2 точки, b2, k2");
+    Console.Write("b2 = ");
+    double d = Convert.ToDouble(Console.ReadLine());
+    Console.Write("k2 = ");
+    double b = Convert.ToDouble(Console.ReadLine());
+    double x = (d - c) / (a - b);
+    double y = a * x + c;
+    Console.WriteLine($"{x}; {y}");
+}
+Coordinats();
